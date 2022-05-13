2b8cc6e896cc785fdb1a035ddbae1d7b472ef6a2
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-05-13 12:28:05 -0400

    Update CoolExtension.js

--- CoolExtension.js
+++ CoolExtension.js
@@ -258,7 +258,7 @@ if (w == 9) {
     return fetch(fetcherurl).then(response => response.text());
   }
   ifReport(args) {
-    return (args.ONE ? 'args.TWO' : 'args.THREE');
+    return (args.ONE ? args.TWO : args.THREE);
   }
   typeOf(args) {
   return typeof args.ONE;
